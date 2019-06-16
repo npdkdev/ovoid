54d673659c6a6196958ad313673f7c39411dd912
khenop <49522880+khenop@users.noreply.github.com>
2019-06-16 09:06:05 +0700

    Add files via upload

---
+++ src/HTTPClient.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Khenop;
+
+interface HTTPClient
+{
+    /**
+     * send post request
+     *
+     * @param  string                $url
+     * @param  array                 $data
+     * @param  array                 $headers
+     * @return \Stelin\ParseResponse
+     */
+    public function post($url, $data, $headers);
+
+    /**
+     * send post request
+     *
+     * @param  string                $url
+     * @param  array                 $data
+     * @param  array                 $headers
+     * @return \Stelin\ParseResponse
+     */
+    public function get($url, $data, $headers);
+}
