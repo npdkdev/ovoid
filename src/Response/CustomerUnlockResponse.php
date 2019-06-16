ea946005cd840749009cff314a6e5e1874384cf4
khenop <49522880+khenop@users.noreply.github.com>
2019-06-16 09:17:31 +0700

    Add files via upload

---
+++ src/Response/CustomerUnlockResponse.php
@@ -0,0 +1,10 @@
+<?php
+namespace Khenop\Response;
+
+class CustomerUnlockResponse
+{
+    public function __construct($data)
+    {
+        
+    }
+}
---
+++ src/Response/LogoutResponse.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Khenop\Response;
+
+class LogoutResponse
+{
+    public function __construct($data)
+    {
+    }
+}
